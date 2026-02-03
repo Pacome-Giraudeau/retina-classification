2ed5b7cb012d2d700ceb3ee68c869adde0b2d608
Pacôme <153203098+Pacome-Giraudeau@users.noreply.github.com>
2026-02-03 17:20:20 +0100

    test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
 # tests/test_config.py
 import pytest
 import yaml
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.config_manager import ConfigManager
 
 def test_config_file_exists():
